05f37f865ba9436539af1a03d9e0090bc9865d02
Epidra <epidra@web.de>
2021-12-02 14:05:50 +0100

    Port to 1.18.0

--- src/main/java/mod/stairway/StairKeeper.java
+++ src/main/java/mod/stairway/StairKeeper.java
@@ -15,9 +15,9 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 import static mod.stairway.StairwayToAether.MODID;
 
--- src/main/java/mod/stairway/block/BlockChain.java
+++ src/main/java/mod/stairway/block/BlockChain.java
@@ -73,7 +73,7 @@ public class BlockChain extends BlockBase implements SimpleWaterloggedBlock {
 
     public BlockState updateShape(BlockState stateIn, Direction facing, BlockState facingState, LevelAccessor worldIn, BlockPos currentPos, BlockPos facingPos) {
         if (stateIn.getValue(WATERLOGGED)) {
-            worldIn.getLiquidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(worldIn));
+            worldIn.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(worldIn));
         }
         return super.updateShape(stateIn, facing, facingState, worldIn, currentPos, facingPos);
     }
